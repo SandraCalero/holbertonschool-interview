336b7d324f4d272050ec92e2445eb51e16c496f2
Sandra Calero <3358@holbertonschool.com>
2022-04-21 15:31:51 -0500

    ADD lockboxes exercise

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -12,14 +12,14 @@ def canUnlockAll(boxes):
     index_list[0] = True
     isOpen = False
     for box in boxes:
-        isOpen = all(index_list)
-        if isOpen:
-            return True
-        if box == [] and isOpen is False:
-            return False
-        if boxes.index(box) == len(index_list) - 1 and isOpen is False:
-            return False
         for key in box:
             if key < len(index_list):
                 if index_list[key] is False:
                     index_list[key] = True
+            isOpen = all(index_list)
+            if isOpen:
+                return True
+            if box == [] and isOpen is False:
+                return False
+            if boxes.index(box) == len(index_list) - 1 and isOpen is False:
+                return False
