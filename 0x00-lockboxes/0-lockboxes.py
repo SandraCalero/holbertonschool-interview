8b7d679c30a449e148b0c2eae87868e980982591
Sandra Calero <3358@holbertonschool.com>
2022-04-21 18:08:53 -0500

    ADD lockboxes exercise

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -2,17 +2,12 @@
 """Method that determines if all the boxes can be opened"""
 
 
-from sqlalchemy import false
-
-
 def canUnlockAll(boxes):
-    if len(boxes) <= 1:
-        return False
     isOpenList = [False] * (len(boxes) - 1)
     isOpenList[0] = True
     keyList = boxes[0]
     for key in keyList:
-        if (key < len(isOpenList)) and (isOpenList[key] is False):
+        if (key < len(boxes)) and (isOpenList[key] is False):
             isOpenList[key] = True
             keyList.extend(boxes[key])
 
