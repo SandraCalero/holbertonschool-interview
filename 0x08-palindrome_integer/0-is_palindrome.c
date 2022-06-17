1d213d107eecf57667dc5c91620e24fcce518b8b
Sandra Calero <3358@holbertonschool.com>
2022-06-17 15:53:46 -0500

    ADD a function that checks whether or not a given unsigned integer is a palindrom

---
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,26 @@
+#include "palindrome.h"
+/**
+ * is_palindrome - checks whether or not a given unsigned integer
+ * is a palindrome
+ * @n: the number to check
+ *
+ * Return: 1 if n is a palindrome, and 0 otherwise.
+ */
+int is_palindrome(unsigned long n)
+{
+	char num[50];
+	int i, total_digits;
+
+
+	for (i = 0; n; i++)
+	{
+		num[i] = n % 10;
+		n = n / 10;
+	}
+	num[i] = '\0';
+	total_digits = i;
+	for (i = 0; i < (total_digits / 2); i++)
+		if (num[i] != num[total_digits - 1 - i])
+			return (0);
+	return (1);
+}
